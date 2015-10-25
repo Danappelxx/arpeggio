4158b5364dc8ad3032b94cc1fa60439a26306bba
Dan Appel <dan.appel00@gmail.com>
2015-10-25 03:01:13 -0400

    create the basics of the response proccessor

--- models/Response.js
+++ models/Response.js
@@ -9,3 +9,33 @@ Response.attachSchema(new SimpleSchema({
         label: "Response Data"
     }
 }));
+
+
+ChartData = class ChartData {
+    constructor(component, responses) {
+        this.component = component;
+        this.responses = this.proccessResponses();
+    }
+
+    proccessResponses() {
+        // work through them
+        return this.responses
+    }
+}
+
+ResponseProcessor = class ResponseProcessor {
+    constructor(formId) {
+
+        var form = Form.findOne(formId);
+        var componentIds = form.components;
+
+        var chartDatas = componentIds.map ( (componentId) => {
+            var component = Component.findOne({ _id : Components });
+            var responses = Response.find({ componentId : componentId }).fetch();
+
+            return new ChartData(component, responses);
+        });
+
+        this.chartDatas = chartDatas;
+    }
+}
