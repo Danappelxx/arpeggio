1e58672c55287fea36f569d48d47a871539eb21f
Jerry Chen <jerrychen109@gmail.com>
2015-10-25 03:35:37 -0700

    cleaned up hard-coded pie chart

--- client/views/charts/charts.js
+++ client/views/charts/charts.js
@@ -7,47 +7,20 @@ Template.piechart.rendered = function () {
 	var chartData = chartDatas[0];
 
 	var component = chartData.component;
-	var choices = component.data;
-	
-	// dictionary mapping number value to answer choice string
-	var choicesDict = {
 
-	};
-	for (var i = choices.length - 1; i >= 0; i--) {
-		choicesDict[choices[i].value] = choices[i].label;
-	};
+	var choices = component.data;
+	var choicesDict = getChoicesDict(choices);
 
 	var responses = chartData.responses;
+	var answersDict = getAnswersDict(responses);
 
-	// create dictionary with count for each possible response
-	var answersDict = {
-		// "0": 1,
-		// "5": 1,
-		// "10": 2
-	};
-
-	for (var i = responses.length - 1; i >= 0; i--) {
-		var data = responses[i].data;
-		if(answersDict[data] == undefined) {
-			answersDict[data] = 0;
-		}
-		answersDict[responses[i].data]++;
-	};
-
-	// create a matrix to pass in to c3.generate data
-	var columnMatrix = [];
-
-	var keys = Object.keys(answersDict);
-
-	for (var i = keys.length - 1; i >= 0; i--) {
-		columnMatrix.push([choicesDict[keys[i]], answersDict[keys[i]]]);
-	};
+	var dataMatrix = getDataMatrix(choicesDict, answersDict);
 
 	// create pie chart
 	var piechart = c3.generate({
 		bindto: this.find('.piechart'),
 		data: {
-			columns: columnMatrix, // load in formatted matrix?
+			columns: dataMatrix, // load in formatted matrix?
 			type: 'pie'
 		}
 	});
@@ -61,4 +34,47 @@ Template.piechart.rendered = function () {
  //    	]});
  //  	});
 
+}
+
+// dictionary mapping number value to answer choice string
+var getChoicesDict = function (choices) {
+	var choicesDict = {
+
+	};
+	for (var i = choices.length - 1; i >= 0; i--) {
+		choicesDict[choices[i].value] = choices[i].label;
+	};
+
+	return choicesDict;
+};
+
+// create dictionary with count for each possible response
+var getAnswersDict = function (responses) {
+	var answersDict = {
+		// "0": 1,
+		// "5": 1,
+		// "10": 2
+	};
+	for (var i = responses.length - 1; i >= 0; i--) {
+		var data = responses[i].data;
+		if(answersDict[data] == undefined) {
+			answersDict[data] = 0;
+		}
+		answersDict[responses[i].data]++;
+	};
+
+	return answersDict;
+}
+
+// create a matrix to pass in to c3.generate data
+var getDataMatrix = function (choicesDict, answersDict) {
+	var dataMatrix = [];
+
+	var keys = Object.keys(answersDict);
+
+	for (var i = keys.length - 1; i >= 0; i--) {
+		dataMatrix.push([choicesDict[keys[i]], answersDict[keys[i]]]);
+	};
+
+	return dataMatrix;
 }
